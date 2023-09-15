d5dd1473bcd54393084f78281c43cab1a5ccff25
yuktea <73362292+yuktea@users.noreply.github.com>
2023-09-15 17:41:39 +0530

    add algorithm

--- Algorithms/simpleWithSMA.py
+++ Algorithms/simpleWithSMA.py
@@ -4,6 +4,44 @@ from collections import deque
 
 # endregion
 
+class SimpleWithSMA(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2020, 1, 1)
+        self.SetEndDate(2021, 1, 1)
+        self.SetCash(100000)
+        self.spy = self.AddEquity("SPY", Resolution.Daily).Symbol
+        self.sma = DefineSimpleMovingAverage("CustomSMA", 30)
+        self.RegisterIndicator(self.spy, self.sma, Resolution.Daily)
+
+    
+    def OnData(self, data):
+        if not self.sma.IsReady:
+            return
+        
+        hist = self.History(self.spy, timedelta(365), Resolution.Daily)
+        low = min(hist["low"])
+        high = max(hist["high"])
+        
+        price = self.Securities[self.spy].Price
+        
+        if price * 1.05 >= high and self.sma.Current.Value < price:
+            if not self.Portfolio[self.spy].IsLong:
+                self.SetHoldings(self.spy, 1)
+        
+        elif price * 0.95 <= low and self.sma.Current.Value > price:  
+            if not self.Portfolio[self.spy].IsShort:
+                self.SetHoldings(self.spy, -1)
+        
+        else:
+            self.Liquidate()
+        
+        self.Plot("Benchmark", "52w-High", high)
+        self.Plot("Benchmark", "52w-Low", low)
+        self.Plot("Benchmark", "SMA", self.sma.Current.Value)
+
+
+# Custom SMA
 class DefineSimpleMovingAverage(PythonIndicator):
     
     def __init__(self, name, period):
