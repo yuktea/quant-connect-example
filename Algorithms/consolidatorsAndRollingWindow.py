aa138a768822819635d8d46e53a6dcb767132bcb
yuktea <73362292+yuktea@users.noreply.github.com>
2023-09-19 16:56:09 +0530

    add bar handlers and exit

--- Algorithms/consolidatorsAndRollingWindow.py
+++ Algorithms/consolidatorsAndRollingWindow.py
@@ -12,8 +12,29 @@ class ConsolidatorsAndRollingWindow(QCAlgorithm):
         self.SetCash(199000)
         self.symbol = self.AddEquity("SPY", Resolution.Minute).Symbol
         self.rollingWindow = RollingWindow[TradeBar](2)
-        self.Consolidate(self.symbol, Resolution.Daily, self.CustomBarHandler)
+        self.Consolidate(self.symbol, Resolution.Daily, self.BarHandler)
         
         self.Schedule.On(self.DateRules.EveryDay(self.symbol),
                  self.TimeRules.BeforeMarketClose(self.symbol, 15),      
-                 self.ExitPositions)
+                 self.Exit)
+
+    def OnData(self, data):
+        
+        if not self.rollingWindow.IsReady:
+            return
+        
+        if not (self.Time.hour == 9 and self.Time.minute == 31):
+            return
+        
+        # Gap is Up = Sell
+        if data[self.symbol].Open >= 1.01*self.rollingWindow[0].Close:
+            self.SetHoldings(self.symbol, -1)
+        # Gap is Down = Buy
+        elif data[self.symbol].Open <= 0.99*self.rollingWindow[0].Close:
+            self.SetHoldings(self.symbol, 1)
+
+    def BarHandler(self, bar):
+        self.rollingWindow.Add(bar)
+
+    def Exit(self):
+        self.Liquidate(self.symbol)
